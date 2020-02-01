e6ed486958314051be89e9062cebf206af801030
whitelaw-josh <whitelaw.josh@yahoo.com>
2020-01-31 19:13:10 -0500

    Updated methods and code to build additional offensive force units and attack.

--- starcraft_tutorial.py
+++ starcraft_tutorial.py
@@ -3,11 +3,16 @@ import random
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
-    CYBERNETICSCORE, STALKER
+    CYBERNETICSCORE, STARGATE, STALKER, VOIDRAY
 
 #Our BotAI class
 class SentdeBot(sc2.BotAI):
+    def __init__(self):
+        self.ITERATIONS_PER_MINUTE = 165 #Roughly 165 iterations (estimated)
+        self.MAX_WORKERS = 50 #Max workers of 50
+
     async def on_step(self, iteration):
+        self.iteration = iteration
         await self.distribute_workers() #Distribute workers evenly to mineral patches (in sc2/bot_ai.py)
         await self.build_workers()
         await self.build_pylons()
@@ -18,13 +23,16 @@ class SentdeBot(sc2.BotAI):
         await self.attack()
 
     #Method to build workers at NEXUS if affordable
+    #TODO: consider if 16 should be switched to 22 for workers in mineral patches and assimilators
     async def build_workers(self):
-        for nexus in self.units(NEXUS).ready.noqueue:
-            if self.can_afford(PROBE):
-                await self.do(nexus.train(PROBE))
+        if (len(self.units(NEXUS)) * 16) > len(self.units(PROBE)) and len(self.units(PROBE)) < self.MAX_WORKERS:
+            for nexus in self.units(NEXUS).ready.noqueue:
+                if self.can_afford(PROBE):
+                    await self.do(nexus.train(PROBE))
 
     #Method to build pylon if supply left is less than 5 and pylon is not already being built
     #Will build pylon near nexus first and if afforadble
+    #TODO: May need to update location of building pylons
     async def build_pylons(self):
         if self.supply_left < 5 and not self.already_pending(PYLON):
             nexi = self.units(NEXUS).ready
@@ -34,6 +42,7 @@ class SentdeBot(sc2.BotAI):
 
     #Method to build assimilator within 25 units of a nexus and if affordable
     #Find worker that is available and geyser that is available to have assimilator built
+    #TODO: May need to update location of building assimilators
     async def build_assimilators(self):
         for nexus in self.units(NEXUS).ready:
             geysers = self.state.vespene_geyser.closer_than(25.0, nexus)
@@ -55,6 +64,7 @@ class SentdeBot(sc2.BotAI):
             await self.expand_now()
 
     #Method to build offensive buildings (i.e. Race = Protoss, Gateway)
+    #TODO: May need to update location of building offensive force buildings
     async def build_offensive_force_buildings(self):
         if self.units(PYLON).ready.exists:
             pylon = self.units(PYLON).ready.random
@@ -62,15 +72,25 @@ class SentdeBot(sc2.BotAI):
             if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
                 if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
                     await self.build(CYBERNETICSCORE, near=pylon)
-            elif len(self.units(GATEWAY)) < 3:
+            elif len(self.units(GATEWAY)) < (self.iteration / self.ITERATIONS_PER_MINUTE):
                 if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                     await self.build(GATEWAY, near=pylon)
+            
+            if self.units(CYBERNETICSCORE).ready.exists:
+                if len(self.units(STARGATE)) < ((self.iteration / self.ITERATIONS_PER_MINUTE) / 2):
+                    if self.can_afford(STARGATE) and not self.already_pending(STARGATE):
+                        await self.build(STARGATE, near=pylon)
 
     #Method to build offensive units (i.e.  Race = Protoss, Stalkers)
     async def build_offensive_force(self):
         for gw in self.units(GATEWAY).ready.noqueue:
-            if self.can_afford(STALKER) and self.supply_left > 0:
-                await self.do(gw.train(STALKER))
+            if not self.units(STALKER).amount > self.units(VOIDRAY).amount:
+                if self.can_afford(STALKER) and self.supply_left > 0:
+                    await self.do(gw.train(STALKER))
+
+        for sg in self.units(STARGATE).ready.noqueue:
+            if self.can_afford(VOIDRAY) and self.supply_left > 0:
+                await self.do(sg.train(VOIDRAY))
 
     #Method to find targets for offensive units to attack 
     def find_target(self, state):
@@ -83,17 +103,22 @@ class SentdeBot(sc2.BotAI):
 
     #Method to make offensive units attack if more than 3 stalkers and we have seen an enemy
     #First offense, then defense
-    #TODO: Conditional to check if being attacked
+    #TODO: Conditional to check if being attacked (i.e. defend)
     #TODO: Might need to have variable (local or outside of scope of attack method) containing sum of all offensive units
     async def attack(self):
-        if self.units(STALKER).amount > 15:
-            for s in self.units(STALKER).idle:
-                await self.do(s.attack(self.find_target(self.state)))
+        # {UNIT: [n to fight, n to defend]}
+        aggressive_units = {STALKER: [15, 5],
+                            VOIDRAY: [8, 3]}
 
-        elif self.units(STALKER).amount > 3:
-            if len(self.known_enemy_units) > 0:
-                for s in self.units(STALKER).idle:
-                    await self.do(s.attack(random.choice(self.known_enemy_units)))
+        for UNIT in aggressive_units:
+            if self.units(UNIT).amount > aggressive_units[UNIT][0] and self.units(UNIT).amount > aggressive_units[UNIT][1]:
+                for s in self.units(UNIT).idle:
+                    await self.do(s.attack(self.find_target(self.state)))
+
+            elif self.units(UNIT).amount > aggressive_units[UNIT][1]:
+                if len(self.known_enemy_units) > 0:
+                    for s in self.units(UNIT).idle:
+                        await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 #Starts the game we want to run taking into the map and players we wish to add
 #realtime controls speed of how game is played
