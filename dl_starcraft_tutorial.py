86b1a84123dafb731c8ee6bd08f22b19b42bb286
whitelaw-josh <whitelaw.josh@yahoo.com>
2020-02-15 23:27:04 -0500

    Updated attack method, adding way to create training data, etc.

--- dl_starcraft_tutorial.py
+++ dl_starcraft_tutorial.py
@@ -1,17 +1,32 @@
 import cv2
 import numpy as np
+import os
 import random
+import time
 import sc2
-from sc2 import run_game, maps, Race, Difficulty, position
+from sc2 import run_game, maps, Race, Difficulty, position, Result
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, \
     CYBERNETICSCORE, STARGATE, ROBOTICSFACILITY, STALKER, VOIDRAY, OBSERVER
+
+#os.environ["SC2PATH"] = 'C:\Program Files (x86)\StarCraft II'
+
+HEADLESS = False
 
 #Our BotAI class
 class SentdeBot(sc2.BotAI):
     def __init__(self):
         self.ITERATIONS_PER_MINUTE = 165 #Roughly 165 iterations (estimated)
         self.MAX_WORKERS = 50 #Max workers of 50
+        self.do_something_after = 0 #Wait time to make another decision (iterations)
+        self.train_data = []
+
+    def on_end(self, game_result):
+        print('---- on_end called ----')
+        print(game_result)
+
+        if game_result == Result.Victory:
+            np.save("C:/Program Files (x86)/StarCraft II/train_data/{}.npy".format(str(int(time.time()))), np.array(self.train_data))
 
     async def on_step(self, iteration):
         self.iteration = iteration
@@ -153,7 +168,29 @@ class SentdeBot(sc2.BotAI):
 
         for obs in self.units(OBSERVER).ready:
             cv2.circle(game_data, (int(obs.position[0]), int(obs.position[1])), 1, (255, 255, 255), -1)
-            
+   
+        line_max = 50
+        mineral_ratio = self.minerals / 1500
+        vespene_ratio = self.vespene / 1500
+        population_ratio = self.supply_left / self.supply_cap
+        plausible_supply = self.supply_cap / 200.0
+        military_weight = len(self.units(VOIDRAY)) / (self.supply_cap-self.supply_left) #TODO: Update this based on different army compositions
+
+        if mineral_ratio > 1.0:
+            mineral_ratio = 1.0           
+        if vespene_ratio > 1.0:
+            vespene_ratio = 1.0       
+        if population_ratio > 1.0:
+            population_ratio = 1.0           
+        if military_weight > 1.0:
+            military_weight = 1.0
+
+        cv2.line(game_data, (0, 19), (int(line_max*military_weight), 19), (250, 250, 200), 3)  # worker/supply ratio
+        cv2.line(game_data, (0, 15), (int(line_max*plausible_supply), 15), (220, 200, 200), 3)  # plausible supply (supply/200.0)
+        cv2.line(game_data, (0, 11), (int(line_max*population_ratio), 11), (150, 150, 150), 3)  # population ratio (supply_left/supply)
+        cv2.line(game_data, (0, 7), (int(line_max*vespene_ratio), 7), (210, 200, 0), 3)  # gas / 1500
+        cv2.line(game_data, (0, 3), (int(line_max*mineral_ratio), 3), (0, 255, 25), 3)  # minerals minerals/1500
+
         main_base_names = ["NEXUS", "COMMANDERCENTER", "HATCHERY"]
 
         for enemy_building in self.known_enemy_structures:
@@ -170,12 +207,13 @@ class SentdeBot(sc2.BotAI):
                     cv2.circle(game_data, (int(enemy_unit.position[0]), int(enemy_unit.position[1])), 1, (55, 0, 155), -1)
                 else:
                     cv2.circle(game_data, (int(enemy_unit.position[0]), int(enemy_unit.position[1])), 3, (55, 0, 215), -1)
+        
+        self.flipped = cv2.flip(game_data, 0) #Coordinate system needs flipped
 
-        flipped = cv2.flip(game_data, 0) #Coordinate system needs flipped
-        resized = cv2.resize(flipped, dsize=None, fx=2, fy=2)
-
-        cv2.imshow('Intel', resized)
-        cv2.waitKey(1)
+        if not HEADLESS:
+            resized = cv2.resize(self.flipped, dsize=None, fx=2, fy=2)
+            cv2.imshow('Intel', resized)
+            cv2.waitKey(1)
 
     #Method to find targets for offensive units to attack 
     def find_target(self, state):
@@ -190,13 +228,31 @@ class SentdeBot(sc2.BotAI):
     #TODO: Conditional to check if being attacked (i.e. defend)
     #TODO: Might need to have variable (local or outside of scope of attack method) containing sum of all offensive units
     async def attack(self):
-        # {UNIT: [n to fight, n to defend]}
-        aggressive_units = {STALKER: [15, 5],
-                            VOIDRAY: [8, 3]}
+        if len(self.units(VOIDRAY).idle) > 0:
+            choice = random.randrange(0, 4)
+            target = False
 
-        for UNIT in aggressive_units:
-                for s in self.units(UNIT).idle:
-                    await self.do(s.attack(self.find_target(self.state))) 
+            if self.iteration > self.do_something_after:
+                if choice == 0: #Do not attack
+                    wait = random.randrange(20, 165)
+                    self.do_something_after = self.iteration + wait
+            elif choice == 1: #Attack units closest our nexus (random)
+                if len(self.known_enemy_units) > 0:
+                    target = self.known_enemy_units.closest_to(random.choice(self.units(NEXUS))) #TODO: Update choice of nexus
+            elif choice == 2: #Attack enemy structures if known
+                if len(self.known_enemy_structures) > 0:
+                    target = random.choice(self.known_enemy_structures)
+            elif choice == 3: #Attack enemy start location
+                target = self.enemy_start_locations[0]
+
+            if target:
+                for vr in self.units(VOIDRAY).idle:
+                    await self.do(vr.attack(target))
+            
+            y = np.zeros(4)
+            y[choice] = 1
+            #print(y)
+            self.train_data.append([y, self.flipped])
 
 #Starts the game we want to run taking into the map and players we wish to add
 #realtime controls speed of how game is played
